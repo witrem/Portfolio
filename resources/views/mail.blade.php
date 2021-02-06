68485dd9511b8dd4a8fab981f00875293935562a
Alexis <alexislegeay@hotmail.fr>
2021-02-06 17:32:46 +0100

    design mail

--- resources/views/mail.blade.php
+++ resources/views/mail.blade.php
@@ -1,6 +1,10 @@
 <html>
+<body>
 <h1>Contact de {{ $data['nom']}} {{ $data['prenom']}}</h1>
 <br>Mail : {{$data['mail']}}
 <br>Message : {{$data['message']}}
-
+<footer class="footer">
+    @include('footer')
+    </footer>
+</body>
 </html>
